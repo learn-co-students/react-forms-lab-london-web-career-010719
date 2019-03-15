b8945a4b0249ce433a2ca160a4bfe909caa53e9c
julesow <souleymanesow@gmail.com>
2019-03-15 20:42:06 +0000

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,26 +4,60 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: '',
+      password: '',
+    };
   }
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+  handleSubmit=(event)=>{
+    event.preventDefault();
+    const username = this.state.username
+    const password = this.state.password;
 
+    if (username.length === 0 || password.length === 0) {
+      window.alert('fields cannot be empty')
+    } else {
+      this.props.onSubmit(this.state);
+    }
+    
+    
+
+  }
   render() {
     return (
-      <form>
+      <form onSubmit={event => this.handleSubmit(event)}>
+       
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input
+              id="username"
+              name="username"
+              type="text"
+              value={this.state.value}
+              onChange={this.handleChange}
+            />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input 
+            id="password" 
+            name="password" 
+            type="password"
+            value={this.state.value}
+            onChange={this.handleChange}
+               />
           </label>
         </div>
         <div>
-          <button type="submit">Log in</button>
+          <button type="submit" onSubmit={this.handleSubmit}>Log in</button>
         </div>
       </form>
     );
